4c9d9ed71359b64b3c35c5bb098053a2dfe44995
Rishabh srivastava <rishabhvastava75@gmail.com>
2026-09-13 04:23:21 +0530

    Allow Vercel frontend in CORS

--- Employee Management/src/main/java/com/rishabh/employeemanagement/config/SecurityConfig.java
+++ Employee Management/src/main/java/com/rishabh/employeemanagement/config/SecurityConfig.java
@@ -5,14 +5,16 @@ import com.rishabh.employeemanagement.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import org.springframework.security.web.authentication.
-        UsernamePasswordAuthenticationFilter;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -29,13 +31,16 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+
                 .cors(cors -> {})
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
                 )
+
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(
                                 SessionCreationPolicy.STATELESS
@@ -50,4 +55,36 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration =
+                new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+                "https://employee-management-omega-umber.vercel.app"
+        ));
+
+        configuration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                configuration
+        );
+
+        return source;
+    }
 }
